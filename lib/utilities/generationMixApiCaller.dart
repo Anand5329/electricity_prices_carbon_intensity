b55db118a86bf2334ed25d2639c364b0bdd6c2f0
Anand Doshi <anand.doshi20@imperial.ac.uk>
2025-07-22 16:02:57 +0100

    [feat] Added regional gen mix capability, and testing for each energy source being present.

--- lib/utilities/generationMixApiCaller.dart
+++ lib/utilities/generationMixApiCaller.dart
@@ -17,13 +17,19 @@ class GenerationMixApiCaller extends ApiCaller {
   Future<PeriodData<GenerationMix>> getCurrentGenerationMix() async {
     final response = await getRaw(_generation);
     if (!isValidResponse(response)) {
-      throw Exception("Could not get current generation mix.\nError code: ${response.statusCode}\n${response.body}");
+      throw Exception(
+        "Could not get current generation mix.\nError code: ${response.statusCode}\n${response.body}",
+      );
     }
     final json = jsonDecode(response.body);
     return _parseOne(json);
   }
 
-  Future<List<PeriodData<GenerationMix>>> getGenerationMixFrom(DateTime from, {FromModifier modifier = FromModifier.past24, DateTime? to}) async {
+  Future<List<PeriodData<GenerationMix>>> getGenerationMixFrom(
+    DateTime from, {
+    FromModifier modifier = FromModifier.past24,
+    DateTime? to,
+  }) async {
     String modifierStr = "";
     switch (modifier) {
       case FromModifier.past24:
@@ -35,12 +41,19 @@ class GenerationMixApiCaller extends ApiCaller {
         }
         modifierStr = "${to.toIso8601String()}/";
         break;
-      default: throw Exception("Invalid argument modifier: expected one of {FromModifier.past24, FromModifier.to}");
+      default:
+        throw Exception(
+          "Invalid argument modifier: expected one of {FromModifier.past24, FromModifier.to}",
+        );
     }
 
-    final response = await getRaw("$_generation${from.toIso8601String()}/$modifierStr");
+    final response = await getRaw(
+      "$_generation${from.toIso8601String()}/$modifierStr",
+    );
     if (!isValidResponse(response)) {
-      throw Exception("Could not get generation mix.\nError code: ${response.statusCode}\n${response.body}");
+      throw Exception(
+        "Could not get generation mix.\nError code: ${response.statusCode}\n${response.body}",
+      );
     }
     final json = jsonDecode(response.body);
     return _parseList(json);
@@ -48,25 +61,58 @@ class GenerationMixApiCaller extends ApiCaller {
 
   List<PeriodData<GenerationMix>> _parseList(Map<String, dynamic> json) {
     List data = json["data"];
-    return data.map((jsonElem) => parseTimePeriod(jsonElem, GenerationMix.fromJson)).toList();
+    return data
+        .map((jsonElem) => parseTimePeriod(jsonElem, GenerationMix.fromJson))
+        .toList();
   }
 
   PeriodData<GenerationMix> _parseOne(Map<String, dynamic> json) {
     Map<String, dynamic> data = json["data"];
     return parseTimePeriod(data, GenerationMix.fromJson);
   }
+}
 
+enum EnergySource {
+  biomass("biomass"),
+  imports("imports"),
+  gas("gas"),
+  nuclear("nuclear"),
+  other("other"),
+  hydro("hydro"),
+  solar("solar"),
+  wind("wind"),
+  coal("coal");
+
+  final String _type;
+  const EnergySource(this._type);
+
+  bool equals(other) {
+    return other is EnergySource && other._type == _type;
+  }
+
+  static EnergySource fromString(String source) {
+    return EnergySource.values.reduce((src1, src2) {
+      if (src1._type == source) {
+        return src1;
+      }
+      if (src2._type == source) {
+        return src2;
+      }
+      // should never happen
+      return EnergySource.other;
+    });
+  }
 }
 
 /// to store each generation factor within the generation mix
 class GenerationFactor {
   /// The fuel type contributing to the generation
-  final String fuel;
+  final EnergySource energySource;
 
   /// The percentage of generation mix denoted by this fuel type
   final double perc;
 
-  GenerationFactor(this.fuel, this.perc);
+  GenerationFactor(this.energySource, this.perc);
 
   factory GenerationFactor.fromJson(Map<String, dynamic> json) {
     double perc = 0;
@@ -76,32 +122,31 @@ class GenerationFactor {
     } else {
       perc = percJson;
     }
-    return GenerationFactor(json["fuel"], perc);
+    return GenerationFactor(EnergySource.fromString(json["fuel"]), perc);
   }
 
   @override
   bool operator ==(Object other) {
     return other is GenerationFactor &&
-        this.fuel == other.fuel &&
+        this.energySource == other.energySource &&
         this.perc == other.perc;
   }
 
   @override
-  int get hashCode => fuel.hashCode + (perc * 31).round();
+  int get hashCode => energySource.hashCode + (perc * 31).round();
 }
 
 /// to store the temporal generation mix
 class GenerationMix implements Comparable<GenerationMix> {
   final List<GenerationFactor> factors;
 
-  GenerationMix({
-    required this.factors,
-  });
+  GenerationMix({required this.factors});
 
-  factory GenerationMix.fromJson(
-    Map<String, dynamic> json) {
+  factory GenerationMix.fromJson(Map<String, dynamic> json) {
     List<dynamic> factors = json["generationmix"];
-    List<GenerationFactor> genFactors = factors.map((factorJson) => GenerationFactor.fromJson(factorJson)).toList();
+    List<GenerationFactor> genFactors = factors
+        .map((factorJson) => GenerationFactor.fromJson(factorJson))
+        .toList();
     GenerationMix genMix = GenerationMix(factors: genFactors);
     return genMix;
   }
--- test/utilities/generationMixApi_test.dart
+++ test/utilities/generationMixApi_test.dart
@@ -1,7 +1,7 @@
-
 import 'package:electricity_prices_and_carbon_intensity/utilities/carbonIntensityApiCaller.dart';
 import 'package:electricity_prices_and_carbon_intensity/utilities/generationMixApiCaller.dart';
 import 'package:electricity_prices_and_carbon_intensity/utilities/httpclient.dart';
+import 'package:electricity_prices_and_carbon_intensity/utilities/regionalCarbonIntensityGenerationMixApiCaller.dart';
 import 'package:test/test.dart';
 import 'package:logger/logger.dart';
 
@@ -15,6 +15,10 @@ void main() {
       DateTime now = DateTime.now().toUtc();
 
       expect(period.value.factors.isNotEmpty, true);
+      expect(
+        Set.of(period.value.factors.map((factor) => factor.energySource)),
+        Set.of(EnergySource.values),
+      );
       expect(period.from.isBefore(now), true);
       expect(period.to.isBefore(now), true);
     });
@@ -22,16 +26,55 @@ void main() {
     test("testing generation mix from and to date", () async {
       DateTime from = DateTime(2023, 3, 9, 16, 20);
       DateTime to = DateTime(2023, 3, 9, 19, 20);
-      List<PeriodData<GenerationMix>> data = await caller.getGenerationMixFrom(from, modifier: FromModifier.to, to: to);
+      List<PeriodData<GenerationMix>> data = await caller.getGenerationMixFrom(
+        from,
+        modifier: FromModifier.to,
+        to: to,
+      );
 
       expect(data.length, 6);
     });
 
     test("testing generation mix from and past 24h", () async {
       DateTime from = DateTime(2023, 3, 9, 16, 20);
-      List<PeriodData<GenerationMix>> data = await caller.getGenerationMixFrom(from, modifier: FromModifier.past24);
+      List<PeriodData<GenerationMix>> data = await caller.getGenerationMixFrom(
+        from,
+        modifier: FromModifier.past24,
+      );
 
       expect(data.length, 48);
+    });
+  });
+
+  RegionalCarbonIntensityGenerationMixCaller regionalCaller =
+      RegionalCarbonIntensityGenerationMixCaller();
+  group("Testing regional generation mix by postcode", () {
+    test("get current regional generation mix", () async {
+      RegionalData regionalData = await regionalCaller
+          .getRegionalDataForPostcode("N1");
+      expect(regionalData.generationData.isNotEmpty, true);
+      expect(
+        Set.of(
+          regionalData.generationData.first.value.factors.map(
+            (factor) => factor.energySource,
+          ),
+        ),
+        Set.of(EnergySource.values),
+      );
+    });
+
+    test("get current regional generation mix by regionid", () async {
+      RegionalData regionalData = await regionalCaller
+          .getRegionalDataForRegionId(13);
+      expect(regionalData.generationData.isNotEmpty, true);
+      expect(
+        Set.of(
+          regionalData.generationData.first.value.factors.map(
+            (factor) => factor.energySource,
+          ),
+        ),
+        Set.of(EnergySource.values),
+      );
     });
   });
 }
