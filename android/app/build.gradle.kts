bb3b911c770143ca9fa41ffb58696ebe66289620
Anand Doshi <anand.doshi20@imperial.ac.uk>
2025-08-07 11:46:54 +0100

    [feat] Added release information such as icons and upload keystore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,5 @@
-
+import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
@@ -6,6 +7,12 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     kotlin("plugin.serialization") version "2.2.0"
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -37,11 +44,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -68,6 +84,8 @@ dependencies {
 
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("com.github.tony19:logback-android:3.0.0")
+
+    implementation("com.google.android.material:material:1.12.0");
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation(platform("org.junit:junit-bom:5.13.3"))
